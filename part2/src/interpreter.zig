0235c408cb102c8adf7be68f98683d0f54afcce9
Dhole <dhole@riseup.net>
2021-08-14 17:05:02 +0200

    Complete 8.1

--- part2/src/interpreter.zig
+++ part2/src/interpreter.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const expr = @import("expr.zig");
+const stmt = @import("stmt.zig");
 const token = @import("token.zig");
 const helpers = @import("helpers.zig");
 
@@ -14,6 +15,7 @@ const Value = expr.Value;
 const ValueTag = expr.ValueTag;
 const Unary = expr.Unary;
 const Binary = expr.Binary;
+const Stmt = stmt.Stmt;
 
 const Error = error{RuntimeError};
 
@@ -28,145 +30,171 @@ pub const Context = struct {
     }
 };
 
-fn isTruthy(val: Value) bool {
-    return switch (val) {
-        .nil => false,
-        .boolean => |v| v,
-        else => true,
-    };
-}
+pub const Interpreter = struct {
+    const Self = @This();
 
-fn isEqual(a: Value, b: Value) bool {
-    if (@as(ValueTag, a) != @as(ValueTag, b)) {
-        return false;
+    pub fn init() Self {
+        return Self{};
     }
-    return switch (a) {
-        .boolean => |va| va == b.boolean,
-        .number => |va| va == b.number,
-        .string => |va| std.mem.eql(u8, va, b.string),
-        .nil => true,
-    };
-}
 
-fn checkNumberOperand(c: *Context, operator: Token, operand: Value) error{RuntimeError}!void {
-    if (operand == ValueTag.number) {
+    pub fn deinit(self: *Self) void {
+        _ = self;
         return;
     }
-    c.err = .{ .tok = operator, .msg = "Operand must be a number." };
-    return error.RuntimeError;
-}
 
-fn checkNumberOperands(c: *Context, operator: Token, left: Value, right: Value) error{RuntimeError}!void {
-    if (left == ValueTag.number and right == ValueTag.number) {
-        return;
+    fn isTruthy(val: Value) bool {
+        return switch (val) {
+            .nil => false,
+            .boolean => |v| v,
+            else => true,
+        };
     }
-    c.err = .{ .tok = operator, .msg = "Operands must be numbers." };
-    return error.RuntimeError;
-}
 
-fn evalUnary(c: *Context, exp: *const Unary) Error!Value {
-    const right = try eval(c, exp.right);
-
-    switch (exp.operator.type) {
-        TT.MINUS => {
-            try checkNumberOperand(c, exp.operator, right);
-            return Value{ .number = -(right.number) };
-        },
-        TT.BANG => {
-            return Value{ .boolean = !isTruthy(right) };
-        },
-        else => unreachable,
+    fn isEqual(a: Value, b: Value) bool {
+        if (@as(ValueTag, a) != @as(ValueTag, b)) {
+            return false;
+        }
+        return switch (a) {
+            .boolean => |va| va == b.boolean,
+            .number => |va| va == b.number,
+            .string => |va| std.mem.eql(u8, va, b.string),
+            .nil => true,
+        };
     }
-}
 
-fn evalBinary(c: *Context, exp: *const Binary) Error!Value {
-    const left = try eval(c, exp.left);
-    const right = try eval(c, exp.right);
-
-    switch (exp.operator.type) {
-        TT.MINUS => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .number = (left.number) - (right.number) };
-        },
-        TT.SLASH => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .number = (left.number) / (right.number) };
-        },
-        TT.STAR => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .number = (left.number) * (right.number) };
-        },
-        TT.PLUS => {
-            if (left == .number and right == .number) {
-                return Value{ .number = (left.number) + (right.number) };
-            }
-            if (left == .string and right == .string) {
-                @panic("'string + string' not yet implemented");
-            }
-            c.err = .{ .tok = exp.operator, .msg = "Operands must be two numbers or two strings." };
-            return error.RuntimeError;
-        },
-        TT.GREATER => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .boolean = (left.number) > (right.number) };
-        },
-        TT.GREATER_EQUAL => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .boolean = (left.number) >= (right.number) };
-        },
-        TT.LESS => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .boolean = (left.number) < (right.number) };
-        },
-        TT.LESS_EQUAL => {
-            try checkNumberOperands(c, exp.operator, left, right);
-            return Value{ .boolean = (left.number) <= (right.number) };
-        },
-        TT.BANG_EQUAL => {
-            return Value{ .boolean = !isEqual(left, right) };
-        },
-        TT.EQUAL_EQUAL => {
-            return Value{ .boolean = isEqual(left, right) };
-        },
-        else => unreachable,
+    fn checkNumberOperand(c: *Context, operator: Token, operand: Value) error{RuntimeError}!void {
+        if (operand == ValueTag.number) {
+            return;
+        }
+        c.err = .{ .tok = operator, .msg = "Operand must be a number." };
+        return error.RuntimeError;
     }
-}
 
-pub fn eval(c: *Context, exp: *const Expr) Error!Value {
-    return switch (exp.*) {
-        .binary => |*e| try evalBinary(c, e),
-        .grouping => |*e| try eval(c, e.expression),
-        .literal => |*e| e.value,
-        .unary => |*e| try evalUnary(c, e),
-    };
-}
-
-pub fn stringify(w: anytype, val: Value) !void {
-    const min_i64 = std.math.minInt(i64);
-    const max_i64 = std.math.maxInt(i64);
-    switch (val) {
-        .boolean => |v| try w.print("{s}", .{v}),
-        .number => |v| {
-            if (min_i64 <= v and v <= max_i64 and (v - @intToFloat(f64, @floatToInt(i64, v))) == 0.0) {
-                try w.print("{d:.0}", .{v});
-            } else {
-                try w.print("{d}", .{v});
-            }
-        },
-        .string => |v| try w.print("{s}", .{v}),
-        .nil => try w.print("nil", .{}),
+    fn checkNumberOperands(c: *Context, operator: Token, left: Value, right: Value) error{RuntimeError}!void {
+        if (left == ValueTag.number and right == ValueTag.number) {
+            return;
+        }
+        c.err = .{ .tok = operator, .msg = "Operands must be numbers." };
+        return error.RuntimeError;
     }
-}
 
-pub fn interpret(w: anytype, exp: *const Expr) !void {
-    var ctx: Context = Context.init();
-    const val = eval(&ctx, exp) catch {
-        reportRuntimeError(ctx.err.?);
-        return;
-    };
-    try stringify(w, val);
-    try w.print("\n", .{});
-}
+    fn evalUnary(self: *Self, c: *Context, exp: *const Unary) Error!Value {
+        const right = try self.eval(c, exp.right);
+
+        switch (exp.operator.type) {
+            TT.MINUS => {
+                try checkNumberOperand(c, exp.operator, right);
+                return Value{ .number = -(right.number) };
+            },
+            TT.BANG => {
+                return Value{ .boolean = !isTruthy(right) };
+            },
+            else => unreachable,
+        }
+    }
+
+    fn evalBinary(self: *Self, c: *Context, exp: *const Binary) Error!Value {
+        const left = try self.eval(c, exp.left);
+        const right = try self.eval(c, exp.right);
+
+        switch (exp.operator.type) {
+            TT.MINUS => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .number = (left.number) - (right.number) };
+            },
+            TT.SLASH => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .number = (left.number) / (right.number) };
+            },
+            TT.STAR => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .number = (left.number) * (right.number) };
+            },
+            TT.PLUS => {
+                if (left == .number and right == .number) {
+                    return Value{ .number = (left.number) + (right.number) };
+                }
+                if (left == .string and right == .string) {
+                    @panic("'string + string' not yet implemented");
+                }
+                c.err = .{ .tok = exp.operator, .msg = "Operands must be two numbers or two strings." };
+                return error.RuntimeError;
+            },
+            TT.GREATER => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .boolean = (left.number) > (right.number) };
+            },
+            TT.GREATER_EQUAL => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .boolean = (left.number) >= (right.number) };
+            },
+            TT.LESS => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .boolean = (left.number) < (right.number) };
+            },
+            TT.LESS_EQUAL => {
+                try checkNumberOperands(c, exp.operator, left, right);
+                return Value{ .boolean = (left.number) <= (right.number) };
+            },
+            TT.BANG_EQUAL => {
+                return Value{ .boolean = !isEqual(left, right) };
+            },
+            TT.EQUAL_EQUAL => {
+                return Value{ .boolean = isEqual(left, right) };
+            },
+            else => unreachable,
+        }
+    }
+
+    pub fn eval(self: *Self, c: *Context, exp: *const Expr) Error!Value {
+        return switch (exp.*) {
+            .binary => |*e| try self.evalBinary(c, e),
+            .grouping => |*e| try self.eval(c, e.expression),
+            .literal => |*e| e.value,
+            .unary => |*e| try self.evalUnary(c, e),
+        };
+    }
+
+    pub fn exec(self: *Self, c: *Context, w: anytype, stm: *const Stmt) !void {
+        switch (stm.*) {
+            .expression => |e| {
+                _ = try self.eval(c, e);
+            },
+            .print => |e| {
+                const val = try self.eval(c, e);
+                try stringify(w, val);
+                try w.print("\n", .{});
+            },
+        }
+    }
+
+    pub fn stringify(w: anytype, val: Value) !void {
+        const min_i64 = std.math.minInt(i64);
+        const max_i64 = std.math.maxInt(i64);
+        switch (val) {
+            .boolean => |v| try w.print("{s}", .{v}),
+            .number => |v| {
+                if (min_i64 <= v and v <= max_i64 and (v - @intToFloat(f64, @floatToInt(i64, v))) == 0.0) {
+                    try w.print("{d:.0}", .{v});
+                } else {
+                    try w.print("{d}", .{v});
+                }
+            },
+            .string => |v| try w.print("{s}", .{v}),
+            .nil => try w.print("nil", .{}),
+        }
+    }
+
+    pub fn interpret(self: *Self, w: anytype, stms: []const Stmt) !void {
+        var ctx: Context = Context.init();
+        for (stms) |*stm| {
+            self.exec(&ctx, w, stm) catch {
+                reportRuntimeError(ctx.err.?);
+                return;
+            };
+        }
+    }
+};
 
 test "interpreter" {
     const scanner = @import("scanner.zig");
@@ -176,20 +204,29 @@ test "interpreter" {
     const Parser = parser.Parser;
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var s = try Scanner.init(&gpa.allocator, "(1 + 2 * 3 - 4)");
+    var s = try Scanner.init(&gpa.allocator, "(1 + 2 * 3 - 4);");
     defer s.deinit();
     var tokens = try s.scanTokens();
     var p = Parser.init(&gpa.allocator, tokens);
     defer p.deinit();
-    var exp = p.parse().?;
+    var statements = try p.parse();
 
-    var ctx: Context = Context.init();
-    var val = eval(&ctx, exp) catch |e| {
-        std.debug.panic("err: {s}, ctx: {s}", .{ e, ctx });
-    };
-    std.debug.print("{s}\n", .{val});
+    // var ctx: Context = Context.init();
+    var int = Interpreter.init();
+    defer int.deinit();
     var w = std.io.getStdErr().writer();
-    try stringify(w, val);
+    try int.interpret(w, statements.items);
+    // for (statements.items) |*stm| {
+    //     exec(&ctx, w, stm) catch |e| {
+    //         std.debug.panic("err: {s}, ctx: {s}", .{ e, ctx });
+    //     };
+    // }
+    // var val = exec(&ctx, w, exp) catch |e| {
+    //     std.debug.panic("err: {s}, ctx: {s}", .{ e, ctx });
+    // };
+    // std.debug.print("{s}\n", .{val});
+    // var w = std.io.getStdErr().writer();
+    // try stringify(w, val);
     // std.debug.print("{d}\n", .{@field(val, "boolean")});
     // std.debug.print("{d}\n", .{val.number});
 }
--- part2/src/main.zig
+++ part2/src/main.zig
@@ -9,7 +9,7 @@ const interpreter = @import("interpreter.zig");
 
 const Expr = expr.Expr;
 const Parser = parser.Parser;
-const interpret = interpreter.interpret;
+const Interpreter = interpreter.Interpreter;
 
 const MAX_FILE_SIZE = 0x1000000;
 
@@ -17,77 +17,93 @@ var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 pub fn main() !u8 {
     const args = std.os.argv[1..std.os.argv.len];
+    var m = Main.init();
+    defer m.deinit();
     if (args.len > 1) {
         std.log.err("Usage: jlox [script]", .{});
         return 64;
     } else if (args.len == 1) {
         const arg0 = std.mem.sliceTo(args[0], '0');
-        return try runFile(arg0);
+        return try m.runFile(arg0);
     } else {
-        return try runPrompt();
+        return try m.runPrompt();
     }
     return 0;
 }
 
-fn runFile(path: []u8) !u8 {
-    std.log.info("path: {s}", .{path});
+const Main = struct {
+    const Self = @This();
 
-    var path_file = try std.fs.cwd().openFile(path, .{ .read = true });
-    const bytes = try path_file.readToEndAlloc(&gpa.allocator, MAX_FILE_SIZE);
-    defer gpa.allocator.free(bytes);
+    interpreter: Interpreter,
 
-    try run(bytes);
-
-    if (helpers.hadError) {
-        return 65;
+    fn init() Self {
+        return Self{
+            .interpreter = Interpreter.init(),
+        };
     }
-    if (helpers.hadRuntimeError) {
-        return 70;
+
+    fn deinit(self: *Self) void {
+        self.interpreter.deinit();
     }
-    return 0;
-}
 
-fn runPrompt() !u8 {
-    const stdout = std.io.getStdOut();
-    const stdin = std.io.getStdIn();
-    const reader = std.io.bufferedReader(stdin.reader()).reader();
+    fn runFile(self: *Self, path: []u8) !u8 {
+        std.log.info("path: {s}", .{path});
 
-    var buffer: [1024]u8 = undefined;
-    while (true) {
-        try stdout.writeAll("> ");
-        const line = try reader.readUntilDelimiterOrEof(&buffer, '\n');
-        if (line) |l| {
-            try run(l);
-            helpers.hadError = false;
-            helpers.hadRuntimeError = false;
-        } else {
-            return 0;
+        var path_file = try std.fs.cwd().openFile(path, .{ .read = true });
+        const bytes = try path_file.readToEndAlloc(&gpa.allocator, MAX_FILE_SIZE);
+        defer gpa.allocator.free(bytes);
+
+        try self.run(bytes);
+
+        if (helpers.hadError) {
+            return 65;
+        }
+        if (helpers.hadRuntimeError) {
+            return 70;
+        }
+        return 0;
+    }
+
+    fn runPrompt(self: *Self) !u8 {
+        const stdout = std.io.getStdOut();
+        const stdin = std.io.getStdIn();
+        const reader = std.io.bufferedReader(stdin.reader()).reader();
+
+        var buffer: [1024]u8 = undefined;
+        while (true) {
+            try stdout.writeAll("> ");
+            const line = try reader.readUntilDelimiterOrEof(&buffer, '\n');
+            if (line) |l| {
+                try self.run(l);
+                helpers.hadError = false;
+                helpers.hadRuntimeError = false;
+            } else {
+                return 0;
+            }
         }
     }
-}
 
-fn run(bytes: []u8) !void {
-    // std.log.info("{any}", .{bytes});
-    var s = try scanner.Scanner.init(&gpa.allocator, bytes);
-    defer s.deinit();
-    const tokens = try s.scanTokens();
+    fn run(self: *Self, bytes: []u8) !void {
+        // std.log.info("{any}", .{bytes});
+        var s = try scanner.Scanner.init(&gpa.allocator, bytes);
+        defer s.deinit();
+        const tokens = try s.scanTokens();
 
-    var exp: *Expr = undefined;
-    var p = Parser.init(&gpa.allocator, tokens);
-    defer p.deinit();
+        var p = Parser.init(&gpa.allocator, tokens);
+        defer p.deinit();
 
-    if (p.parse()) |e| {
-        exp = e;
-    } else {
-        return;
+        var statements = p.parse() catch |e| {
+            switch (e) {
+                error.ParseError => return,
+                else => return e,
+            }
+        };
+
+        if (helpers.hadError) {
+            return;
+        }
+
+        var w = std.io.getStdOut().writer();
+        try self.interpreter.interpret(w, statements.items);
     }
-
-    if (helpers.hadError) {
-        return;
-    }
-
-    var w = std.io.getStdOut().writer();
-    try interpret(w, exp);
-    // try expr.printAst(w, exp);
-
-}
+};
